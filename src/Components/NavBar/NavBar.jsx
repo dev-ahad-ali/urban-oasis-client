b7106e828746ce7ec24deb9c9de6307a6b804873
dev-ahad-ali <ahadali.dev@gmail.com>
2024-06-04 00:10:03 +0600

    logo added

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import SideMenu from './SIdeMenu/SideMenu';
 import { Button } from '@material-tailwind/react';
 import { CgMenuRightAlt } from 'react-icons/cg';
+import logo from '../../assets/img/logo.png';
 
 const NavBar = () => {
   const [open, setOpen] = useState(false);
@@ -10,18 +11,26 @@ const NavBar = () => {
   const closeSideMenu = () => setOpen(false);
 
   return (
-    <nav>
-      <div>
-        <Button
-          onClick={openSideMenu}
-          variant='text'
-          size='lg'
-          className='font-title flex items-center gap-1 text-lg font-medium capitalize'
-        >
-          <CgMenuRightAlt />
-          Menu
-        </Button>
-        <SideMenu open={open} closeSideMenu={closeSideMenu} />
+    <nav className='border-customBlack border-b-2 py-2'>
+      <div className='flex items-center justify-between px-5'>
+        <div>
+          <Button
+            onClick={openSideMenu}
+            variant='text'
+            size='lg'
+            className='font-title border-customBlack flex items-center gap-1 rounded-none border-r-2 p-0 px-5 py-3 text-lg font-medium capitalize'
+          >
+            <CgMenuRightAlt />
+            Menu
+          </Button>
+          <SideMenu open={open} closeSideMenu={closeSideMenu} />
+        </div>
+        <div>
+          <img src={logo} alt='logo' className='h-[50px]' />
+        </div>
+        <div>
+          <Button>Login</Button>
+        </div>
       </div>
     </nav>
   );
