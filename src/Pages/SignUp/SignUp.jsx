c6fd05c6afb715effb792f65a94b759da227822d
dev-ahad-ali <ahadali.dev@gmail.com>
2024-06-05 00:13:22 +0600

    sign up functionality added

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -3,9 +3,18 @@ import { useForm } from 'react-hook-form';
 import { BsFillEyeFill, BsFillEyeSlashFill } from 'react-icons/bs';
 import { MdHome } from 'react-icons/md';
 import { Link } from 'react-router-dom';
+import useAuth from '../../Hooks/useAuth';
+import useAxiosPublic from '../../Hooks/useAxiosPublic';
+import { toast } from 'react-toastify';
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const SignUp = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const { createUser, updateUser } = useAuth();
+  const axiosPublic = useAxiosPublic();
+
   const {
     register,
     handleSubmit,
@@ -13,8 +22,35 @@ const SignUp = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = async (data) => {
+    const imageFile = { image: data.image[0] };
+    // console.log(imageFile);
+
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+      headers: { 'content-type': 'multipart/form-data' },
+    });
+
+    if (res.data.success) {
+      const userInfo = {
+        name: data.name,
+        email: data.email,
+        profileImage: res.data.data.display_url,
+        role: 'user',
+      };
+
+      const userRes = await createUser(data.email, data.password);
+
+      if (userRes.user) {
+        updateUser(data.name, res.data.data.display_url).then(() => {
+          axiosPublic.post('/users', userInfo).then((res) => {
+            if (res.data.insertedId) {
+              reset();
+              toast.success('User created successfully');
+            }
+          });
+        });
+      }
+    }
   };
 
   return (
@@ -95,10 +131,10 @@ const SignUp = () => {
                         return 'Password must include 1 uppercase letter';
                       }
                     },
-                    includeLowercase: (v) => {
-                      const pattern = /(?=.*[a-z])/;
+                    includeSpecialCharacters: (v) => {
+                      const pattern = /[^a-zA-Z0-9_]/;
                       if (!pattern.test(v)) {
-                        return 'Password must include 1 lowercase letter';
+                        return 'Password must include 1 special character';
                       }
                     },
                   },
@@ -125,7 +161,18 @@ const SignUp = () => {
             <input
               type='file'
               className='block w-full cursor-pointer rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm text-gray-600 placeholder-gray-400/70 file:rounded-full file:border-none file:bg-gray-200 file:px-4 file:py-1 file:text-sm file:text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40'
+              {...register('image', {
+                required: {
+                  value: true,
+                  message: 'Must select a image',
+                },
+              })}
             />
+            {errors.image && (
+              <p className='font-noto text-sm font-semibold text-red-500'>
+                {errors.image.message}
+              </p>
+            )}
             <p className='mt-1 text-center text-sm font-medium'>
               Upload Profile Picture
             </p>
@@ -135,7 +182,7 @@ const SignUp = () => {
           <input
             type='submit'
             value='Sign up'
-            className='w-full rounded bg-blue-500 px-4 py-2 font-bold text-white duration-300'
+            className='w-full cursor-pointer rounded bg-blue-500 px-4 py-2 font-bold text-white duration-300'
           />
         </form>
 
