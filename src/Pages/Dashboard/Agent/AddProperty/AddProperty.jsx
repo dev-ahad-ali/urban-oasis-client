168667bf9834520516c4005a470d44b2ee5da60b
dev-ahad-ali <ahadali.dev@gmail.com>
2024-06-08 20:28:32 +0600

    property image upload functionalities added

--- src/Pages/Dashboard/Agent/AddProperty/AddProperty.jsx
+++ src/Pages/Dashboard/Agent/AddProperty/AddProperty.jsx
@@ -1,77 +1,196 @@
-import { Button, Input } from '@material-tailwind/react';
+import { Button, Input, Textarea } from '@material-tailwind/react';
 import useAuth from '../../../../Hooks/useAuth';
+import { useForm } from 'react-hook-form';
+import useAxiosPublic from '../../../../Hooks/useAxiosPublic';
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const AddProperty = () => {
   const { user } = useAuth();
+  const axiosPublic = useAxiosPublic();
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const handleAddProperty = async (data) => {
+    const imageFile = { image: data.image[0] };
+
+    const imageRes = await axiosPublic.post(image_hosting_api, imageFile, {
+      headers: { 'content-type': 'multipart/form-data' },
+    });
+
+    if (imageRes.data.success) {
+      const property = {
+        title: data.title,
+        location: data.location,
+        description: data.description,
+        minPrice: data.minPrice,
+        maxPrice: data.maxPrice,
+        agentEmail: user?.email,
+        agentName: user?.displayName,
+        image: imageRes.data.data.display_url,
+        status: 'pending',
+      };
+      console.log(property);
+    }
+    reset();
+  };
 
   return (
     <section className='min-h-full'>
       <h2 className='text-3xl'>AddProperty</h2>
 
       <div className='mt-12 min-h-full'>
-        <form className='flex flex-col gap-10'>
+        <form
+          onSubmit={handleSubmit(handleAddProperty)}
+          className='flex flex-col gap-10'
+        >
           <div>
             <Input
               color='black'
               variant='standard'
               label='Property Title'
               placeholder='Property Title'
+              {...register('title', {
+                required: {
+                  value: true,
+                  message: 'Please fill up this field',
+                },
+              })}
             />
+            {errors.title && (
+              <p className='text-sm font-semibold text-red-500'>
+                {errors.title.message}
+              </p>
+            )}
           </div>
           <div className='grid grid-cols-2 gap-5'>
-            <Input
-              color='black'
-              variant='standard'
-              label='Location'
-              placeholder='Location'
-            />
+            <div>
+              <Input
+                color='black'
+                variant='standard'
+                label='Location'
+                placeholder='Location'
+                {...register('location', {
+                  required: {
+                    value: true,
+                    message: 'Please fill up this field',
+                  },
+                })}
+              />
+              {errors.location && (
+                <p className='text-sm font-semibold text-red-500'>
+                  {errors.location.message}
+                </p>
+              )}
+            </div>
             <div className='grid grid-cols-2 gap-2'>
-              <Input
-                type='number'
-                color='black'
-                variant='standard'
-                label='Minimum Price'
-                placeholder='Minimum Price'
-              />
-              <Input
-                type='number'
-                color='black'
-                variant='standard'
-                label='Maximum Price'
-                placeholder='Maximum Price'
-              />
+              <div>
+                <Input
+                  type='number'
+                  color='black'
+                  variant='standard'
+                  label='Minimum Price'
+                  placeholder='Minimum Price'
+                  {...register('minPrice', {
+                    required: {
+                      value: true,
+                      message: 'Please fill up this field',
+                    },
+                  })}
+                />
+                {errors.minPrice && (
+                  <p className='text-sm font-semibold text-red-500'>
+                    {errors.minPrice.message}
+                  </p>
+                )}
+              </div>
+              <div>
+                <Input
+                  type='number'
+                  color='black'
+                  variant='standard'
+                  label='Maximum Price'
+                  placeholder='Maximum Price'
+                  {...register('maxPrice', {
+                    required: {
+                      value: true,
+                      message: 'Please fill up this field',
+                    },
+                  })}
+                />
+                {errors.maxPrice && (
+                  <p className='text-sm font-semibold text-red-500'>
+                    {errors.maxPrice.message}
+                  </p>
+                )}
+              </div>
             </div>
           </div>
-          <div>
-            <label
-              htmlFor='dropzone-file'
-              className='mx-auto mt-2 flex w-full max-w-lg cursor-pointer flex-col items-center rounded-xl border-2 border-dashed border-gray-300 bg-white p-5 text-center dark:border-gray-700 dark:bg-gray-900'
-            >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth='1.5'
-                stroke='currentColor'
-                className='h-8 w-8 text-gray-500 dark:text-gray-400'
+          <div className='grid grid-cols-2 gap-5'>
+            <div>
+              <Textarea
+                label='Property Description'
+                className='h-[140px] w-full'
+                {...register('description', {
+                  required: {
+                    value: true,
+                    message: 'Please fill up this field',
+                  },
+                })}
+              />
+              {errors.description && (
+                <p className='text-sm font-semibold text-red-500'>
+                  {errors.description.message}
+                </p>
+              )}
+            </div>
+            <div>
+              <label
+                htmlFor='dropzone-file'
+                className='mx-auto flex w-full max-w-lg cursor-pointer flex-col items-center rounded-xl border-2 border-dashed border-gray-300 bg-white p-5 text-center dark:border-gray-700 dark:bg-gray-900'
               >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z'
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  strokeWidth='1.5'
+                  stroke='currentColor'
+                  className='h-8 w-8 text-gray-500 dark:text-gray-400'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z'
+                  />
+                </svg>
+
+                <h2 className='-mt-1 font-medium tracking-wide text-gray-700 dark:text-gray-200'>
+                  Property Image
+                </h2>
+                <input
+                  id='dropzone-file'
+                  type='file'
+                  className='block w-full cursor-pointer rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm text-gray-600 placeholder-gray-400/70 file:rounded-full file:border-none file:bg-gray-200 file:px-4 file:py-1 file:text-sm file:text-gray-700'
+                  {...register('image', {
+                    required: {
+                      value: true,
+                      message: 'Must select a image',
+                    },
+                  })}
                 />
-              </svg>
-
-              <h2 className='mt-1 font-medium tracking-wide text-gray-700 dark:text-gray-200'>
-                Property Image
-              </h2>
-
-              <p className='mt-2 text-xs tracking-wide text-gray-500 dark:text-gray-400'>
-                Upload or darg & drop your file.
-              </p>
-
-              <input id='dropzone-file' type='file' className='hidden' />
-            </label>
+              </label>
+              {errors.image && (
+                <p className='text-sm font-semibold text-red-500'>
+                  {errors.image.message}
+                </p>
+              )}
+            </div>
           </div>
           <div className='grid grid-cols-2 gap-5'>
             <Input
