80a04d25b2c6a0c7107f85df5f295c30f49056c3
dev-ahad-ali <ahadali.dev@gmail.com>
2024-06-11 17:57:53 +0600

    handle wish list function added

--- src/Pages/PropertyDetails/PropertyDetails.jsx
+++ src/Pages/PropertyDetails/PropertyDetails.jsx
@@ -2,14 +2,30 @@ import { Button, Typography } from '@material-tailwind/react';
 import { useParams } from 'react-router-dom';
 import LoadingSpinner from '../../Components/LoadingSpinner/LoadingSpinner';
 import usePropertyData from '../../Hooks/usePropertyData';
+import useAuth from '../../Hooks/useAuth';
+import useAxiosPublic from '../../Hooks/useAxiosPublic';
+import { toast } from 'react-toastify';
 
 const PropertyDetails = () => {
   const { id } = useParams();
   const { property, propertyPending } = usePropertyData(id);
+  const { user } = useAuth();
+  const axiosPublic = useAxiosPublic();
 
   const { title, image } = property;
 
-  const handleWishList = () => {};
+  const handleWishList = async (id) => {
+    const wishDetails = {
+      propertyID: id,
+      userName: user?.displayName,
+      userEmail: user?.email,
+    };
+
+    const wishRes = await axiosPublic.post('/wishList', wishDetails);
+    if (wishRes.data.insertedId) {
+      toast.success('Property added to wish list.');
+    }
+  };
 
   if (propertyPending) {
     return <LoadingSpinner />;
@@ -19,11 +35,17 @@ const PropertyDetails = () => {
     <div>
       <figure>
         <img className='h-96 w-full rounded-lg object-cover object-center' src={image} />
-        <Typography as='caption' variant='small' className='mt-2 text-center font-normal'>
+        <Typography as='p' variant='small' className='mt-2 text-center font-normal'>
           {title}
         </Typography>
         <div>
-          <Button>Add To wishlist</Button>
+          <Button
+            onClick={() => {
+              handleWishList(id);
+            }}
+          >
+            Add To wishlist
+          </Button>
         </div>
       </figure>
     </div>
