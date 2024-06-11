5997dc16bbcbab22a62025fd34789710affd3c13
dev-ahad-ali <ahadali.dev@gmail.com>
2024-06-11 14:47:40 +0600

    shown all verified properties

--- src/Pages/AllProperties/AllProperties.jsx
+++ src/Pages/AllProperties/AllProperties.jsx
@@ -1,7 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
+import useAxiosSecure from '../../Hooks/useAxiosSecure';
+import LoadingSpinner from '../../Components/LoadingSpinner/LoadingSpinner';
+
 const AllProperties = () => {
+  const axiosSecure = useAxiosSecure();
+
+  const { data: allProperties = [], isPending: propertiesPending } = useQuery({
+    queryKey: ['allProperties'],
+    queryFn: async () => {
+      const propertiesRes = await axiosSecure.get('/allProperties');
+      return propertiesRes.data;
+    },
+  });
+
+  if (propertiesPending) {
+    return <LoadingSpinner />;
+  }
+
   return (
     <div>
       <h2 className='text-3xl'>ALL properties</h2>
+      <div>
+        {allProperties.map(({ title, _id }) => (
+          <p key={_id}>{title}</p>
+        ))}
+      </div>
     </div>
   );
 };
