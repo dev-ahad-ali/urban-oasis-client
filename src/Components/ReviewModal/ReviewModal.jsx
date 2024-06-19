8878440f5323a9ceafd8c6d5231759093a09f166
dev-ahad-ali <ahadali.dev@gmail.com>
2024-06-19 20:03:14 +0600

    review modal added

---
+++ src/Components/ReviewModal/ReviewModal.jsx
@@ -0,0 +1,69 @@
+import { Button, Dialog, DialogBody, DialogHeader, Rating } from '@material-tailwind/react';
+import usePropertyData from '../../Hooks/usePropertyData';
+import useAuth from '../../Hooks/useAuth';
+import { MdRateReview } from 'react-icons/md';
+import { useState } from 'react';
+
+const ReviewModal = ({ open, handleOpen, propertyId }) => {
+  const { property } = usePropertyData(propertyId);
+  const { user } = useAuth();
+  const { title, agentName } = property;
+  const date = new Date().toDateString();
+  const [rating, setRating] = useState(0);
+
+  const handleReview = () => {
+    const reviewMessage = document.getElementById('reviewMessage').value;
+
+    const review = {
+      reviewerImage: user?.photoURL,
+      reviewerName: user?.displayName,
+      reviewerEmail: user?.email,
+      reviewMessage: reviewMessage,
+      reviewRating: rating,
+      reviewDate: date,
+      agentName: agentName,
+      propertyId: propertyId,
+      propertyTitle: title,
+    };
+
+    console.log(review);
+  };
+
+  return (
+    <Dialog open={open} className='p-4' size='sm'>
+      <DialogHeader className='p-0 text-xl font-medium'>Give a review for : {title}</DialogHeader>
+      <div className='my-2 flex justify-center'>
+        <Rating value={rating} onChange={setRating} />
+      </div>
+      <DialogBody className='p-0'>
+        <p>Message:</p>
+        <textarea
+          id='reviewMessage'
+          className='h-[130px] w-full resize-none rounded-md border border-customBlack p-2'
+        ></textarea>
+      </DialogBody>
+      <p className='text-end text-sm'>Current Date : {date}</p>
+      <div className='mt-4 flex w-full items-center gap-3 p-0'>
+        <Button
+          color='red'
+          size='sm'
+          onClick={() => handleOpen(false)}
+          className='w-full font-title capitalize'
+        >
+          <span>Cancel</span>
+        </Button>
+        <Button
+          size='sm'
+          color='blue'
+          onClick={handleReview}
+          className='flex w-full items-center justify-center gap-1 font-title capitalize'
+        >
+          Post
+          <MdRateReview className='mt-[2px]' />
+        </Button>
+      </div>
+    </Dialog>
+  );
+};
+
+export default ReviewModal;
--- src/Pages/PropertyDetails/PropertyDetails.jsx
+++ src/Pages/PropertyDetails/PropertyDetails.jsx
@@ -1,4 +1,4 @@
-import { Button, Typography } from '@material-tailwind/react';
+import { Button } from '@material-tailwind/react';
 import { useParams } from 'react-router-dom';
 import LoadingSpinner from '../../Components/LoadingSpinner/LoadingSpinner';
 import usePropertyData from '../../Hooks/usePropertyData';
@@ -8,12 +8,15 @@ import { toast } from 'react-toastify';
 import { PiArrowsLeftRight } from 'react-icons/pi';
 import { FaHeart } from 'react-icons/fa';
 import { MdRateReview } from 'react-icons/md';
+import { useState } from 'react';
+import ReviewModal from '../../Components/ReviewModal/ReviewModal';
 
 const PropertyDetails = () => {
   const { id } = useParams();
   const { property, propertyPending } = usePropertyData(id);
   const { user } = useAuth();
   const axiosPublic = useAxiosPublic();
+  const [open, handleOpen] = useState(false);
 
   const {
     title,
@@ -46,76 +49,81 @@ const PropertyDetails = () => {
   }
 
   return (
-    <section className='px-7'>
-      <div className='h-52'></div>
-      {/* title */}
-      <div>
-        <h2 className='font-title text-8xl'>{title}`</h2>
-        <div className='flex items-center justify-between gap-3 border-b-2 border-customBlack py-4'>
-          <p>{description.slice(0, 50)}.......</p>
-          <p className='text-lg font-semibold text-gray-500'>{location}</p>
-        </div>
-      </div>
-      {/* image */}
-      <img className='mt-5 h-[700px] w-full' src={image} />
-      {/* details */}
-      <div className='my-12 flex items-center justify-around gap-3 rounded-full bg-white p-4 shadow-xl'>
+    <>
+      <section className='px-7'>
+        <div className='h-52'></div>
+        {/* title */}
         <div>
-          <p className='font-title text-2xl font-semibold'>Location : </p>
-          <p className='text-xl italic'>{location}</p>
+          <h2 className='font-title text-8xl'>{title}`</h2>
+          <div className='flex items-center justify-between gap-3 border-b-2 border-customBlack py-4'>
+            <p>{description.slice(0, 50)}.......</p>
+            <p className='text-lg font-semibold text-gray-500'>{location}</p>
+          </div>
         </div>
-        <div>
-          <p className='font-title text-2xl font-semibold'>Price Range : </p>
-          <p className='flex items-center gap-2 text-xl italic'>
-            ${minPrice}
-            <PiArrowsLeftRight />${maxPrice}
-          </p>
+        {/* image */}
+        <img className='mt-5 h-[700px] w-full' src={image} />
+        {/* details */}
+        <div className='my-12 flex items-center justify-around gap-3 rounded-full bg-white p-4 shadow-xl'>
+          <div>
+            <p className='font-title text-2xl font-semibold'>Location : </p>
+            <p className='text-xl italic'>{location}</p>
+          </div>
+          <div>
+            <p className='font-title text-2xl font-semibold'>Price Range : </p>
+            <p className='flex items-center gap-2 text-xl italic'>
+              ${minPrice}
+              <PiArrowsLeftRight />${maxPrice}
+            </p>
+          </div>
+          <div>
+            <p className='font-title text-2xl font-semibold'>Status : </p>
+            <p className='text-xl italic'>{status}</p>
+          </div>
         </div>
-        <div>
-          <p className='font-title text-2xl font-semibold'>Status : </p>
-          <p className='text-xl italic'>{status}</p>
-        </div>
-      </div>
-      <div className='grid grid-cols-2 items-center gap-5'>
-        <div>
-          <h3 className='mb-4 font-title text-2xl font-semibold'>Property Description : </h3>
-          <p className='text-xl text-gray-600'>{description}</p>
-        </div>
-        {/* agent details */}
-        <div>
-          <div className='grid place-items-center'>
-            <h3 className='mb-4 font-title text-2xl font-semibold'>Property Listed By : </h3>
-            <div className='item-center flex gap-2 rounded-xl border border-customBlack p-4'>
-              <img src={agentImage} alt='' className='w-12 rounded-md' />
-              <div>
-                <p className='text-lg'>{agentName}</p>
-                <p className='font-semibold'>{agentEmail}</p>
+        <div className='grid grid-cols-2 items-center gap-5'>
+          <div>
+            <h3 className='mb-4 font-title text-2xl font-semibold'>Property Description : </h3>
+            <p className='text-xl text-gray-600'>{description}</p>
+          </div>
+          {/* agent details */}
+          <div>
+            <div className='grid place-items-center'>
+              <h3 className='mb-4 font-title text-2xl font-semibold'>Property Listed By : </h3>
+              <div className='item-center flex gap-2 rounded-xl border border-customBlack p-4'>
+                <img src={agentImage} alt='' className='w-12 rounded-md' />
+                <div>
+                  <p className='text-lg'>{agentName}</p>
+                  <p className='font-semibold'>{agentEmail}</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      {/* Add review or add to wishlist */}
-      <div className='my-16 grid grid-cols-2 items-center gap-5'>
-        <Button
-          color='blue'
-          className='flex items-center justify-center gap-4 font-title text-lg capitalize'
-        >
-          Write a review
-          <MdRateReview className='mt-[2px] text-xl' />
-        </Button>
-        <Button
-          color='pink'
-          className='flex items-center justify-center gap-4 font-title text-lg capitalize'
-          onClick={() => {
-            handleWishList(id);
-          }}
-        >
-          Add To wishlist
-          <FaHeart />
-        </Button>
-      </div>
-    </section>
+        {/* Add review or add to wishlist */}
+        <div className='my-16 grid grid-cols-2 items-center gap-5'>
+          <Button
+            onClick={() => handleOpen(true)}
+            color='blue'
+            className='flex items-center justify-center gap-4 font-title text-lg capitalize'
+          >
+            Write a review
+            <MdRateReview className='mt-[2px] text-xl' />
+          </Button>
+          <Button
+            color='pink'
+            className='flex items-center justify-center gap-4 font-title text-lg capitalize'
+            onClick={() => {
+              handleWishList(id);
+            }}
+          >
+            Add To wishlist
+            <FaHeart />
+          </Button>
+        </div>
+      </section>
+      {/* Review Modal */}
+      <ReviewModal propertyId={id} open={open} handleOpen={handleOpen} />
+    </>
   );
 };
 
