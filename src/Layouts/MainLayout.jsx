5557d8e0fc16ceb8ad2fb99bf60619a9845e9cc9
dev-ahad-ali <ahadali.dev@gmail.com>
2024-06-22 20:11:14 +0600

    footer added

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -2,12 +2,14 @@ import { Outlet } from 'react-router-dom';
 import NavBar from '../Components/NavBar/NavBar';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Footer from '../Components/Footer/Footer';
 
 const MainLayout = () => {
   return (
     <>
       <NavBar />
       <Outlet />
+      <Footer />
       <ToastContainer />
     </>
   );
