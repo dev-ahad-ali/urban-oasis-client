bf1494841b651b5809a698509d8a231cd7dfc649
dev-ahad-ali <ahadali.dev@gmail.com>
2024-06-22 20:51:38 +0600

    dashboard profile added

--- src/Pages/Dashboard/User/UserProfile/UserProfile.jsx
+++ src/Pages/Dashboard/User/UserProfile/UserProfile.jsx
@@ -1,7 +1,28 @@
+import useAuth from '../../../../Hooks/useAuth';
+import useRole from '../../../../Hooks/useRole';
+
 const UserProfile = () => {
+  const { user } = useAuth();
+  const { userRole } = useRole(user.email);
   return (
     <div>
-      <h2 className='text-3xl'>User Profile</h2>
+      <h2 className='mb-12 border-b-2 border-customBlack pb-4 font-title text-4xl'>User Profile</h2>
+      <div className='flex items-center gap-12'>
+        <div>
+          <div className='h-[360px] w-[400px]'>
+            <img className='w-full' src={user.photoURL} alt='' />
+          </div>
+        </div>
+        <div className='flex flex-col gap-4'>
+          <h4 className='font-title text-xl font-semibold'>Name : {user.displayName}</h4>
+          <h4>Email : {user.email}</h4>
+          {userRole !== 'user' && (
+            <h4 className='bg-green-400 px-4 py-2 font-title font-bold capitalize text-white'>
+              Special Role : {userRole}
+            </h4>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
